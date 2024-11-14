1fc415e904853cc4505d6709f45964aa4a9157e8
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-11-14 12:08:36 +0900

    feat: display userinfo in Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -16,6 +16,9 @@ const backendPort = process.env.REACT_APP_BACKEND_PORT;
 
 const Profile = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [username, setUsername] = useState('');
+  const [profilePhotoURI, setProfilePhotoURI] = useState('');
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -25,6 +28,9 @@ const Profile = () => {
         .then(response => {
           if (response.status == 200) {
             setIsLoggedIn(true);
+            setUserId(response.data.account.user_id);
+            setUsername(response.data.account.user_name);
+            // setProfilePhotoURI(response.data.profile_photo);
           }
           else {
             setIsLoggedIn(false);
@@ -59,28 +65,30 @@ const Profile = () => {
           <div class="card-body">
             <form>
               <div class="row" style={{ marginBottom: '25px', textAlign: 'left' }}>
-                <div class="col-sm-4 col-md-4 col-lg-3 col-xl-2 col-xxl-2" style={{ display: 'inline', textAlign: 'center', marginBottom: '25px' }}><img class="rounded-circle mb-3 mt-4 img-fluid" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" style={{ display: 'inline', maxHeight: '110px' }} /><br /><button class="btn btn-primary btn-sm" id="photoBtn" type="button">Change Photo</button></div>
+                <div class="col-sm-4 col-md-4 col-lg-3 col-xl-2 col-xxl-2" style={{ display: 'inline', textAlign: 'center', marginBottom: '25px' }}><img class="rounded-circle mb-3 mt-4 img-fluid" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" style={{ display: 'inline', maxHeight: '110px' }} /><br /><button class="btn btn-primary btn-sm" id="photoBtn" type="button">사진 변경</button></div>
                 <div class="col-sm-8 col-md-8 col-lg-9 col-xl-10 col-xxl-10 align-self-center">
                   <div class="row">
                     <div class="col-md-12 text-start">
-                      <div class="mb-3"><label class="form-label" for="email"><strong>ID</strong></label><input class="form-control" type="email" id="email" placeholder="user@example.com" name="email" /></div>
+                      <div class="mb-3"><label class="form-label" for="user_id"><strong>ID</strong></label><input class="form-control" type="text" id="user_id" name="user_id" value={userId} disabled /></div>
                     </div>
                     <div class="col-md-12 text-start">
-                      <div class="mb-3"><label class="form-label" for="username"><strong>닉네임</strong></label><input class="form-control" type="text" placeholder="Username" name="username" /></div>
+                      <div class="mb-3"><label class="form-label" for="user_name"><strong>닉네임</strong></label><input class="form-control" type="text" id="user_name" name="user_name" value={username} /></div>
                     </div>
                   </div>
                 </div>
                 <div class="col-md-6 text-start">
-                  <div class="mb-3"><label class="form-label" for="username"><strong>새 비밀번호</strong></label><input class="form-control" type="password" id="password" placeholder="Password" /></div>
+                  <div class="mb-3"><label class="form-label" for="password"><strong>새 비밀번호</strong></label><input class="form-control" type="password" id="password" placeholder="Password" /></div>
                 </div>
                 <div class="col-md-6 text-start">
-                  <div class="mb-3"><label class="form-label" for="username"><strong>새 비밀번호 확인</strong></label><input class="form-control" type="password" id="verifyPassword" placeholder="Password" /></div>
+                  <div class="mb-3"><label class="form-label" for="password_re"><strong>새 비밀번호 확인</strong></label><input class="form-control" type="password" id="verifyPassword" placeholder="Password" /></div>
                 </div>
                 <div class="col">
                   <p id="emailErrorMsg" class="text-danger" style={{ display: 'none' }}></p>
                   <p id="passwordErrorMsg" class="text-danger" style={{ display: 'none' }}></p>
                 </div>
-                <div class="col-md-12" style={{ textAlign: 'right', marginTop: '5px' }}><button class="btn btn-primary btn-sm" id="submitBtn" type="submit">저장</button></div>
+                <div class="col-md-12" style={{ textAlign: 'right', marginTop: '5px' }}>
+                  <button class="btn btn-primary" id="submitBtn" type="submit">저장</button>
+                </div>
               </div>
             </form>
           </div>
