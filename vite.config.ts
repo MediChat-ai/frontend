81820f3e8e187983a3a0f8f5f13acdda7506ac6e
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2026-01-14 21:17:43 +0900

    fix: The requested module '@vercel/build-utils' does not provide an export named 'getNodeVersion'

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig(({ mode }) => {
   // 환경변수 로드 (VITE_ 접두사 포함)
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    plugins: [vercel(), react()],
+    plugins: [react()],
     server: {
       host: true,
       port: 3000
